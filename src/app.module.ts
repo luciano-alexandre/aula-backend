9feecfc05d240e789b3d79e702f2b85f5f157403
luciano.farias <luciano.cnrn@gmail.com>
2026-04-10 16:12:35 -0300

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProdutosModule } from './produtos/produtos.module';
-import { ProdModule } from './prod/prod.module';
 
 @Module({
-  imports: [ProdutosModule, ProdModule],
+  imports: [ProdutosModule],
   controllers: [AppController],
   providers: [AppService],
 })
